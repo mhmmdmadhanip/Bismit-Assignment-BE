0c253536ed521df08eacff50839b25883c48a13e
mhmmdmadhanip <muhammad.madhani@ui.ac.id>
2024-05-26 17:32:28 +0700

    Add dotenv and env example

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,5 +1,8 @@
 import { INestApplication, Injectable, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
