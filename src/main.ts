3ed9f386cb35b1db63408cf2abab1ccbea9d194c
mhmmdmadhanip <muhammad.madhani@ui.ac.id>
2024-05-29 23:32:02 +0700

    Resolve CORS Problem

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
-  await app.listen(8000);
+  app.enableCors();
+  await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
